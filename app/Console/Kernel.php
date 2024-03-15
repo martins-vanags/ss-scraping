0b087147b9ff665e01d2cfbd50534919a1b7c7b0
Martins <vanagsm4@gmail.com>
2024-03-16 00:56:14 +0200

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,6 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('app:fetch-cars')->dailyAt('23:30');
-
-        $schedule->command('app:yo')->everyMinute();
     }
 
     /**
