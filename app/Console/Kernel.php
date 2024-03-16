12772c78c6a7a91c3aaa8bed53bae62616e270af
Martins <vanagsm4@gmail.com>
2024-03-17 01:01:39 +0200

    wip

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:fetch-cars')->everyFiveMinutes();
+        $schedule->command('app:fetch-cars')->everySixHours();
     }
 
     /**
